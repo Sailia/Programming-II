afa849a21c23e908ba5bbd45c2f112642c78b69e
Skylar <syema.ailia@gmail.com>
2019-10-07 16:20:15 -0500

    complete practice with charAt

---
+++ SeperateDuplicates.java
@@ -0,0 +1,19 @@
+public class SeperateDuplicates
+{
+   public static void main(String[] args)
+   {
+      /*
+      System.out.println(seperateDuplicatesChars("Hello"));
+      System.out.println(seperateDuplicatesChars("Bookkeeper"));
+      System.out.println(seperateDuplicatesChars("Yellowwood door"));
+      System.out.println(seperateDuplicatesChars("Chicago Cubs"));
+      */
+   }
+   
+   public static String seperateDuplicatesChars(String str)
+   {
+      //To be completed
+
+
+   }
+}
