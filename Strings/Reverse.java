318ef47cabff39caf42e8c1d5962daef709ea72a
Skylar <syema.ailia@gmail.com>
2019-10-07 21:10:25 -0500

    rearrange folders

--- Strings/Reverse.java
+++ Strings/Reverse.java
@@ -2,21 +2,28 @@ public class Reverse
 {
    public static void main(String[] args)
    {
-      /*
+      
       System.out.println(isExactReverse("ba", "a"));
       System.out.println(isExactReverse("desserts", "stressed"));
       System.out.println(isExactReverse("apple", "apple"));
       System.out.println(isExactReverse("regal", "lager"));
       System.out.println(isExactReverse("war", "raw"));
       System.out.println(isExactReverse("pal", "slap"));
-      */
+      
 
    }
    public static boolean isExactReverse(String x, String y)
    {
-      //To be completed
+      int count = 1, length = y.length() - 1;
 
-
+      for(int i = 0; i < y.length() - 1; i++) {
+         if(x.charAt(i) == y.charAt(length)) {
+            count++;
+            
+         }
+         length--;
+      }
+      return x.length() == count;
    }
 
 }
