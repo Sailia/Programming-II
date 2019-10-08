a5282a8255ea0325c43e8a9db5c2639c9a373385
Skylar <syema.ailia@gmail.com>
2019-10-07 21:05:48 -0500

    words with hyphens with duplicate letters

--- Strings/SeperateDuplicates.java
+++ Strings/SeperateDuplicates.java
@@ -2,18 +2,22 @@ public class SeperateDuplicates
 {
    public static void main(String[] args)
    {
-      /*
       System.out.println(seperateDuplicatesChars("Hello"));
       System.out.println(seperateDuplicatesChars("Bookkeeper"));
       System.out.println(seperateDuplicatesChars("Yellowwood door"));
       System.out.println(seperateDuplicatesChars("Chicago Cubs"));
-      */
    }
    
    public static String seperateDuplicatesChars(String str)
    {
-      //To be completed
-
-
+      String hyphenated = "";
+      for(int i = 1; i < str.length(); i++) {
+         if(str.charAt(i) == str.charAt(i-1)) {
+            hyphenated += str.charAt(i-1) + "-";
+         } else {
+            hyphenated += str.charAt(i-1);
+         }
+      }
+      return hyphenated += str.charAt(str.length() - 1);
    }
 }
